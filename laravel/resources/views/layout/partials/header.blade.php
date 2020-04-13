1b987bea36a1bb749eac0a57c0652503db157b23
PatricNox <hello@PatricNox.info>
2020-04-13 17:46:32 +0200

    Project: minor SEO adaptions

--- laravel/resources/views/layout/partials/header.blade.php
+++ laravel/resources/views/layout/partials/header.blade.php
@@ -12,10 +12,22 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     {{-- Title --}}
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- TODO - Change based on tenant --}}
+    <title>Kapten Haddock's samlade svordomar | Från Tin Tin</title>
 
     {{-- Meta --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- SEO --}}
+    {{-- TODO - Change based on tenant --}}
+    <meta name="description" content="Kapten Haddock's samlade svordomar!">
+    <meta name=”robots” content="index, follow">
+    <meta property="og:title" content="Haddock's Svordomar">
+    <meta property="og:url" content="{{ config('app.url') }}">
+    <meta property="og:site_name" content="Haddock's Svordomar">
+    <meta property="twitter:title" content="Haddock's Svordomar">
+    <meta property="twitter:description" content="Kapten Haddock's samlade svordomar!">
+    <meta property="og:description" content="Kapten Haddock's samlade svordomar!">
 </head>
 @include('layout.partials.site--loader')
